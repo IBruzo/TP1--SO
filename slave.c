51f41ca700b48af5f8a544c2dd7ab6f138a4576b
cijjas <cijjas@itba.edu.ar>
2023-04-13 20:52:39 -0300

    rebase md5

--- slave.c
+++ slave.c
@@ -38,32 +38,55 @@ int main(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    char buffer[MAX_PATH_SIZE];
+    int childStatus;
+    char buffer[MAX_PATH_SIZE]; 
+
     while ( (read(0,buffer,MAX_PATH_SIZE)) != EOF ){
-        const char *s = "\n"; 
+
+        char bufferCopy[MAX_PATH_SIZE];
+        strncpy(bufferCopy, buffer, MAX_PATH_SIZE);
+        
+        const char *s = "\n";
         char *token;
-        token = strtok(buffer, s);
+        token = strtok(bufferCopy, s);
 
-        int childStatus;
         while( token != NULL ) {
-            // pipe buffer ida : hola.txt
-            // pipe buffer vuelta : pid:3043 dsfgfsad7fg6edf87g6s hola.txt
-            
-            // write "pid getpid()" cheqeuar buffers que van a explotar
-            int forkStatus = fork();
-            if ( forkStatus == 0 ){     // Hijo
-                
-                char * const paramList[] = {"/usr/bin/md5sum", token ,NULL};
+            int pipefd[2]; // fd 3 y 4
+            pipe(pipefd); CHECK_FAIL("pipe");
 
-                execve("/usr/bin/md5sum", paramList, 0); 
+            pid_t pid = fork();
+            if ( pid == 0 ){
+                close(pipefd[0]); 
+                dup2(pipefd[1], STDOUT_FILENO); 
+                close(pipefd[1]); 
+
+                char * const paramList[] = {"/usr/bin/md5sum", token ,NULL};
+                execve("/usr/bin/md5sum", paramList, 0);
                 CHECK_FAIL("execve");
+
+            }else{
+                close(pipefd[1]); 
+
+                char bufferPID[MAX_PATH_SIZE];
+                snprintf(bufferPID, sizeof(bufferPID), "%d", pid);
+
+                char md5Output[MAX_PATH_SIZE];
+                ssize_t numRead = read(pipefd[0], md5Output, MAX_PATH_SIZE);
+                if (numRead == -1) {
+                    perror("read");
+                    exit(EXIT_FAILURE);
+                }
+                md5Output[numRead] = '\0'; 
+                printf("Slave pid : %d\t%s%s", getpid(), bufferPID, md5Output);
+                fflush(stdout);
+
+                close(pipefd[0]);
+                waitpid(pid, &childStatus, 0); 
             }
             token = strtok(NULL, s);
         }
         memset( buffer, 0, sizeof(buffer));
-        while ( waitpid(-1, &childStatus, 0) > 0);
-        
-    }
+    } 
     return 0;
 }
 
